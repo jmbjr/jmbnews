cb1b7d0f7805f28112908b93336fc2b27efa2a52
John Boyle <boylejm@gmail.com>
2019-06-20 20:20:52 -0700

    add some StYlE

--- src/App.js
+++ src/App.js
@@ -27,6 +27,17 @@ const list = [
     objectID: 2
   }
 ];
+const largeColumn = {
+  width: "40%"
+};
+
+const midColumn = {
+  width: "30%"
+};
+
+const smallColumn = {
+  width: "10%"
+};
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -52,10 +63,12 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -75,17 +88,22 @@ const Search = ({ value, onChange, children }) => {
 const Table = ({ list, pattern, onDismiss }) => {
   // do something
   return (
-    <div>
+    <div className="table">
       {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID}>
-          <span>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
             <a href={item.url}>{item.title} </a>
           </span>
-          <span>{item.author} </span>
-          <span>{item.num_comments} </span>
-          <span>{item.points} </span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+          <span style={midColumn}>{item.author} </span>
+          <span style={smallColumn}>{item.num_comments} </span>
+          <span style={smallColumn}>{item.points} </span>
+          <span style={smallColumn}>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
           </span>
         </div>
       ))}
