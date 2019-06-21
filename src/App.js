130dcd86ba44a5b25b0faed718dd9f9d5fc1542f
John Boyle <boylejm@gmail.com>
2019-06-21 00:00:25 -0700

    add API call for news and ensure onDismiss works

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walkddd",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: "BoyleTube",
-    url: "http://boyletube.com",
-    author: "John Boyle",
-    num_comments: 2,
-    points: 10,
-    objectID: 2
-  }
-];
+const DEFAULT_QUERY = "react";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+
 const largeColumn = {
   width: "40%"
 };
@@ -45,8 +24,12 @@ const isSearched = searchTerm => item =>
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { list, searchTerm: "" };
+    this.state = { result: null, searchTerm: DEFAULT_QUERY };
   }
+
+  setSearchTopStories = result => {
+    this.setState({ result });
+  };
 
   onSearchChange = event => {
     this.setState({ searchTerm: event.target.value });
@@ -56,12 +39,27 @@ class App extends Component {
   //use arrow function to avoid having to bind() in constructor
   onDismiss = id => {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
   };
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if (!result) {
+      return null;
+    }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -69,7 +67,11 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
